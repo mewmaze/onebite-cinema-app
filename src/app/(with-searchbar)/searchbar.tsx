fac0706adea31f8d8fd22d529686abd930d17f98
mewmaze <mewmaze@naver.com>
2025-07-04 04:36:01 +0900

    서치바 컴포넌트 생성

---
+++ src/app/(with-searchbar)/searchbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Searchbar() {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const q = searchParams.get("q") || "";
+    setSearch(q);
+  }, [searchParams]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onSubmit = () => {
+    const q = searchParams.get("q") || "";
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "enter") {
+      onSubmit();
+    }
+  };
+  return (
+    <div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+        placeholder="검색어를 입력하세요..."
+      />
+      <button onClick={onSubmit}>검색</button>
+    </div>
+  );
+}
